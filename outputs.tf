b81cfcc528af89a6a295b9386e9d8bb5f84c11af
Caposto <christiand.apostol@outlook.com>
2026-02-28 20:26:48 -0500

    defined basic variables, outputs, and module

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+  description = "ID of dual-stack VPC."
+  value       = dual_stack_vpc.vpc_id
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "Region for dual stack VPC"
+  type        = string
+  default     = "us-east-1"
+}
